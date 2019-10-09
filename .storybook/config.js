7869fa59d8dd0bc5468cbe6cb6024dc5017c9393
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2019-10-09 14:48:26 +0200

    chore: init project

---
+++ .storybook/config.js
@@ -0,0 +1,27 @@
+import { addDecorator, configure, addParameters } from '@storybook/html';
+import { create } from '@storybook/theming';
+import { withKnobs } from '@storybook/addon-knobs';
+
+const req = require.context('../stories', true, /.stories.tsx$/);
+
+addDecorator(withKnobs);
+
+const graviteeTheme = create({
+  base: 'light',
+  brandTitle: 'Gravitee components',
+  brandUrl: 'https://gravitee.io/',
+  brandImage: 'https://graviteesource.com/images/GS_transp.png'
+});
+
+addParameters({
+  options: {
+    enableShortcuts: true,
+    theme: graviteeTheme,
+  },
+});
+
+function loadStories () {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
