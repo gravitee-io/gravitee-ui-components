cc8cecec23d1c86ce403b8d06d4f20d5afec0d81
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2019-11-04 15:20:52 +0100

    refactor: improve stories with knobs

--- stories/atoms/gv-input.stories.js
+++ stories/atoms/gv-input.stories.js
@@ -2,36 +2,38 @@ import '../../src/atoms/gv-input.js';
 import notes from '../../.docs/gv-input.md';
 import { storiesOf } from '@storybook/html';
 import { withActions } from '@storybook/addon-actions';
+import { boolean, text } from '@storybook/addon-knobs';
+import { updateBooleanAttributes, updateTextAttributes } from '../lib/update-attributes';
 
 const eventNames = ['click gv-input'];
 
 storiesOf('Components.Atoms', module)
+  .addParameters({ notes })
   .add('<gv-input>', () => withActions(...eventNames)(() => {
-    return `
-      <div class="title">Type</div>
-      <gv-input type="text"></gv-input>
-      <gv-input type="password" placeholder="Password..."></gv-input>
-      <gv-input type="email" placeholder="Email..."></gv-input>
-      
+
+    const label = text('Label', '');
+    const disabled = boolean('Disabled', false);
+    const skeleton = boolean('Skeleton', false);
+    const required = boolean('Required', false);
+
+    const container = document.createElement('div');
+    container.innerHTML = `
       <div class="title">Size</div>
       <gv-input placeholder="Default size"></gv-input>
       <gv-input large placeholder="Large size"></gv-input>
       <gv-input small placeholder="Small size" ></gv-input>
-    
-      <div class="title">Disabled</div>
-      <gv-input medium disabled></gv-input>
-      <gv-input large disabled></gv-input>
-       <gv-input small disabled></gv-input>
-
-      <div class="title">Required</div>
-      <gv-input placeholder="Required field with placeholder" required></gv-input>
-      <gv-input large required></gv-input>
-      <gv-input small required></gv-input>
-
-      <div class="title">Skeleton</div>
-      <gv-input label="Skeleton field" skeleton></gv-input>
-      <gv-input large skeleton ></gv-input>
-      <gv-input small skeleton ></gv-input>
-   
+      
+      <div class="title">Type</div>
+      <gv-input type="text" placeholder="Text"></gv-input>
+      <gv-input type="password" placeholder="Password..."></gv-input>
+      <gv-input type="email" placeholder="Email..."></gv-input>
     `;
-  }), { notes });
+
+    const nodeList = container.querySelectorAll('gv-input');
+    updateBooleanAttributes(nodeList, 'disabled', disabled);
+    updateBooleanAttributes(nodeList, 'skeleton', skeleton);
+    updateBooleanAttributes(nodeList, 'required', required);
+    updateTextAttributes(nodeList, 'label', label);
+
+    return container;
+  }));
