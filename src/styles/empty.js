6e5bf2346188311bbe7c36bc7061a7901799fa41
Azize Elamrani <azize.elamrani@gmail.com>
2020-04-28 17:02:58 +0200

    feat: User dashboard
    fix gravitee-io/issues#3526

---
+++ src/styles/empty.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { css } from 'lit-element';
+
+// language=CSS
+export const empty = css`
+    .empty, .error {
+      box-sizing: border-box;
+      opacity: 0.5;
+      color: var(--gv-theme-font-color-dark);
+      text-align: center;
+      font-weight: bold;
+      align-items: center;
+      font-size: var(--gv-theme-font-size-xl, 26px);
+      padding: 3rem;
+      height: 100%;
+      width: 100%;
+    }
+`;
