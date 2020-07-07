714f2541fbaca2c9fcbe7bf2d71ffb587e1df81c
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2020-07-07 08:55:43 +0200

    feat: add gv-resizable-views component
    fix gravitee-io/issues#3895

--- src/organisms/gv-resizable-views.js
+++ src/organisms/gv-resizable-views.js
@@ -15,14 +15,166 @@
  */
 import { css, LitElement } from 'lit-element';
 import { html } from 'lit-html';
+import { classMap } from 'lit-html/directives/class-map';
+import { dispatchCustomEvent } from '../lib/events';
 
 /**
  * Resizable views component
  *
+ * @fires gv-resizable-views:resize - when user click to resize action
+ * @fires gv-resizable-views:move - when user move the resize cursor
+ *
  * @slot top - The top content
  * @slot bottom - The bottom content
+ *
+ * @attr {String} direction - the direction [vertical|horizonal]
+ * @attr {Boolean} no-overflow - true if slot containers should have `overflow: hidden`
  */
 export class GvResizableViews extends LitElement {
+
+  static get properties () {
+    return {
+      direction: { type: String, reflect: true },
+      noOverflow: { type: Boolean, attribute: 'no-overflow' },
+    };
+  }
+
+  constructor () {
+    super();
+    this.direction = 'vertical';
+  }
+
+  split () {
+    this.resize();
+  }
+
+  maximizeTop () {
+    this.resize(85, 15);
+  }
+
+  maximizeBottom () {
+    this.resize(20, 80);
+  }
+
+  resize (top = 50, bottom = 50) {
+    this.shadowRoot.querySelector('.top').style.height = `${top}%`;
+    this.shadowRoot.querySelector('.bottom').style.height = `${bottom}%`;
+    // Dispatch event after transition
+    setTimeout(() => {
+      dispatchCustomEvent(this, 'resize', { top, bottom });
+    }, 350);
+
+  }
+
+  resizable (resizer) {
+    const direction = resizer.getAttribute('data-direction') || 'horizontal';
+    const prevSibling = resizer.previousElementSibling;
+    const nextSibling = resizer.nextElementSibling;
+
+    // The current position of mouse
+    let x = 0;
+    let y = 0;
+    let prevSiblingHeight = 0;
+    let prevSiblingWidth = 0;
+    const shadowRoot = this.shadowRoot;
+    const me = this;
+
+    // Handle the mousedown event
+    // that's triggered when user drags the resizer
+    const mouseDownHandler = function (e) {
+      if (!e.target.classList.contains('action')) {
+        // Get the current mouse position
+        shadowRoot.querySelector('.box').classList.remove('transition');
+        x = e.clientX;
+        y = e.clientY;
+        const rect = prevSibling.getBoundingClientRect();
+        prevSiblingHeight = rect.height;
+        prevSiblingWidth = rect.width;
+
+        // Attach the listeners to `document`
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+      }
+    };
+
+    const mouseMoveHandler = function (e) {
+      // How far the mouse has been moved
+      const dx = e.clientX - x;
+      const dy = e.clientY - y;
+      dispatchCustomEvent(me, 'move', { direction });
+      switch (direction) {
+        case 'vertical':
+          prevSibling.style.height = `${(prevSiblingHeight + dy) * 100 / resizer.parentNode.getBoundingClientRect().height}%`;
+          nextSibling.style.height = `${100 - (prevSiblingHeight + dy) * 100 / resizer.parentNode.getBoundingClientRect().height}%`;
+          break;
+        case 'horizontal':
+        default:
+          prevSibling.style.width = `${(prevSiblingWidth + dx) * 100 / resizer.parentNode.getBoundingClientRect().width}%`;
+          break;
+      }
+
+      const cursor = direction === 'horizontal' ? 'col-resize' : 'row-resize';
+      resizer.style.cursor = cursor;
+      resizer.classList.add('drag');
+      document.body.style.cursor = cursor;
+
+      prevSibling.style.userSelect = 'none';
+      prevSibling.style.pointerEvents = 'none';
+
+      nextSibling.style.userSelect = 'none';
+      nextSibling.style.pointerEvents = 'none';
+    };
+
+    const mouseUpHandler = function () {
+      resizer.style.removeProperty('cursor');
+      resizer.classList.remove('drag');
+      document.body.style.removeProperty('cursor');
+
+      prevSibling.style.removeProperty('user-select');
+      prevSibling.style.removeProperty('pointer-events');
+
+      nextSibling.style.removeProperty('user-select');
+      nextSibling.style.removeProperty('pointer-events');
+
+      // Remove the handlers of `mousemove` and `mouseup`
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+
+      shadowRoot.querySelector('.box').classList.add('transition');
+    };
+
+    // Attach the handler
+    resizer.addEventListener('mousedown', mouseDownHandler);
+  };
+
+  firstUpdated () {
+    this.shadowRoot.querySelectorAll('.resizer').forEach((elt) => this.resizable(elt));
+  }
+
+  get shape () {
+    if (this.direction === 'horizontal') {
+      return 'design:vertical';
+    }
+    return 'design:horizontal';
+  }
+
+  render () {
+    return html`
+    <div class="${classMap({ box: true, transition: true, 'no-overflow': this.noOverflow })}">
+      <div class="top"><slot name="top"></slot></div>
+      <div class="resizer" data-direction="${this.direction}">
+        <gv-icon shape="${this.shape}"></gv-icon>
+        <div class="actions">
+          <gv-icon class="layout-icon" shape="layout:layout-right-panel-2"></gv-icon>
+          <gv-button small link class="action" icon="layout:layout-bottom-panel" title="Maximize top" @gv-button:click="${this.maximizeTop}"></gv-button>
+          <gv-button small link class="action" icon="layout:layout-horizontal-2" title="Split screen" @gv-button:click="${this.split}"></gv-button>
+          <gv-button small link class="action" icon="layout:layout-top-panel-6" title="Maximize bottom" @gv-button:click="${this.maximizeBottom}"></gv-button>
+        </div>
+      </div>
+      <div class="bottom"><slot name="bottom"></slot></div>
+    </div>
+    `;
+  }
 
   static get styles () {
     return [
@@ -35,6 +187,14 @@ export class GvResizableViews extends LitElement {
           flex-direction: column;
         }
 
+        :host([direction=horizontal]) .box {
+          flex-direction: row;
+        }
+
+        :host([direction=horizontal]) .layout-icon {
+          display: none;
+        }
+
         * {
           box-sizing: border-box;
         }
@@ -44,7 +204,18 @@ export class GvResizableViews extends LitElement {
           width: 100%;
         }
 
-        ::slotted([slot="bottom"]) {
+        .no-overflow ::slotted(*) {
+          height: 100%;
+          position: relative;
+        }
+
+        .top, .bottom {
+          position: relative;
+        }
+
+        .no-overflow .top,
+        .no-overflow .bottom {
+          overflow: hidden;
         }
 
         .bottom {
@@ -64,6 +235,15 @@ export class GvResizableViews extends LitElement {
         .top, .bottom {
           border-left: 1px solid #BFBFBF;
           border-right: 1px solid #BFBFBF;
+          height: 100%;
+        }
+
+        :host([direction=horizontal]) .top {
+          border-right: 0;
+        }
+
+        :host([direction=horizontal]) .bottom {
+          border-left: 0;
         }
 
         .transition .top, .transition .bottom {
@@ -73,13 +253,19 @@ export class GvResizableViews extends LitElement {
         .top {
           overflow-y: auto;
           overflow-x: hidden;
-          height: 50%;
+          height: 85%;
         }
 
         .bottom {
           overflow-y: auto;
           overflow-x: hidden;
-          height: 50%;
+          height: 15%;
+        }
+
+        :host([direction=horizontal]) .top,
+        :host([direction=horizontal]) .bottom {
+          height: 100%;
+          width: 50%;
         }
 
         .resizer {
@@ -90,10 +276,47 @@ export class GvResizableViews extends LitElement {
           align-items: center;
           justify-content: flex-end;
           --gv-icon--c: #28444F;
+          transition: all .2s ease-in-out;
+          position: relative;
+        }
+
+        .resizer.drag {
+          border-width: 4px 0;
+          border-style: double;
+          border-color: #D9D9D9;
+          background-color: white;
+        }
+
+        .resizer gv-icon,
+        .resizer .actions {
+          transition: all .2s ease-in-out;
+        }
+
+        .resizer.drag gv-icon,
+        .resizer.drag .actions {
+          visibility: hidden;
+          opacity: 0;
+        }
+
+        :host([direction=horizontal]) .resizer.drag {
+          border-width: 0 4px;
         }
 
         .resizer > gv-icon {
-          margin-right: 8px;
+          position: absolute;
+        }
+
+        :host([direction=horizontal]) .resizer {
+          align-items: flex-start;
+        }
+
+        :host([direction=vertical]) .resizer gv-icon {
+          right: 45px;
+        }
+
+        :host([direction=horizontal]) .resizer gv-icon {
+          left: -9px;
+          top: 45px;
         }
 
         .resizer[data-direction="vertical"] {
@@ -146,129 +369,6 @@ export class GvResizableViews extends LitElement {
 
       `,
     ];
-  }
-
-  static get properties () {
-    return {};
-  }
-
-  split () {
-    this.resize('top', '50%');
-    this.resize('bottom', '50%');
-  }
-
-  maximizeTop () {
-    this.resize();
-    this.resize('top', '90%');
-  }
-
-  maximizeBottom () {
-    this.resize('bottom', '80%');
-    this.resize('top', '20%');
-  }
-
-  resize (zone = 'bottom', height = '10%') {
-    this.shadowRoot.querySelector(`.${zone}`).style.height = height;
-  }
-
-  resizable (resizer) {
-    const direction = resizer.getAttribute('data-direction') || 'horizontal';
-    const prevSibling = resizer.previousElementSibling;
-    const nextSibling = resizer.nextElementSibling;
-
-    // The current position of mouse
-    let x = 0;
-    let y = 0;
-    let prevSiblingHeight = 0;
-    let prevSiblingWidth = 0;
-    const shadowRoot = this.shadowRoot;
-
-    // Handle the mousedown event
-    // that's triggered when user drags the resizer
-    const mouseDownHandler = function (e) {
-      if (!e.target.classList.contains('action')) {
-        // Get the current mouse position
-        shadowRoot.querySelector('.box').classList.remove('transition');
-        x = e.clientX;
-        y = e.clientY;
-        const rect = prevSibling.getBoundingClientRect();
-        prevSiblingHeight = rect.height;
-        prevSiblingWidth = rect.width;
-
-        // Attach the listeners to `document`
-        document.addEventListener('mousemove', mouseMoveHandler);
-        document.addEventListener('mouseup', mouseUpHandler);
-      }
-    };
-
-    const mouseMoveHandler = function (e) {
-      // How far the mouse has been moved
-      const dx = e.clientX - x;
-      const dy = e.clientY - y;
-
-      switch (direction) {
-        case 'vertical':
-          prevSibling.style.height = `${(prevSiblingHeight + dy) * 100 / resizer.parentNode.getBoundingClientRect().height}%`;
-          nextSibling.style.height = `${100 - (prevSiblingHeight + dy) * 100 / resizer.parentNode.getBoundingClientRect().height}%`;
-          break;
-        case 'horizontal':
-        default:
-          prevSibling.style.width = `${(prevSiblingWidth + dx) * 100 / resizer.parentNode.getBoundingClientRect().width}%`;
-          break;
-      }
-
-      const cursor = direction === 'horizontal' ? 'col-resize' : 'row-resize';
-      resizer.style.cursor = cursor;
-      document.body.style.cursor = cursor;
-
-      prevSibling.style.userSelect = 'none';
-      prevSibling.style.pointerEvents = 'none';
-
-      nextSibling.style.userSelect = 'none';
-      nextSibling.style.pointerEvents = 'none';
-    };
-
-    const mouseUpHandler = function () {
-      resizer.style.removeProperty('cursor');
-      document.body.style.removeProperty('cursor');
-
-      prevSibling.style.removeProperty('user-select');
-      prevSibling.style.removeProperty('pointer-events');
-
-      nextSibling.style.removeProperty('user-select');
-      nextSibling.style.removeProperty('pointer-events');
-
-      // Remove the handlers of `mousemove` and `mouseup`
-      document.removeEventListener('mousemove', mouseMoveHandler);
-      document.removeEventListener('mouseup', mouseUpHandler);
-
-      shadowRoot.querySelector('.box').classList.add('transition');
-    };
-
-    // Attach the handler
-    resizer.addEventListener('mousedown', mouseDownHandler);
-  };
-
-  firstUpdated () {
-    this.shadowRoot.querySelectorAll('.resizer').forEach((elt) => this.resizable(elt));
-  }
-
-  render () {
-    return html`
-    <div class="box transition">
-      <div class="top"><slot name="top"></slot></div>
-      <div class="resizer" data-direction="vertical">
-        <gv-icon shape="design:horizontal"></gv-icon>
-        <div class="actions">
-          <gv-icon shape="layout:layout-right-panel-2"></gv-icon>
-          <gv-button small link class="action" icon="layout:layout-bottom-panel" title="Maximize top" @gv-button:click="${this.maximizeTop}"></gv-button>
-          <gv-button small link class="action" icon="layout:layout-horizontal-2" title="Split screen" @gv-button:click="${this.split}"></gv-button>
-          <gv-button small link class="action" icon="layout:layout-top-panel-6" title="Maximize bottom" @gv-button:click="${this.maximizeBottom}"></gv-button>
-        </div>
-      </div>
-      <div class="bottom"><slot name="bottom"></slot></div>
-    </div>
-    `;
   }
 }
 
