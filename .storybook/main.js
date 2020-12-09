52e4dc46dfefeb53af4f71ddabdc84c35dcce74e
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2020-12-09 17:44:36 +0100

    chore: improve webpack configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const { merge } = require('webpack-merge');
+
+const maxAssetSize = 1024 * 1024;
 
 module.exports = {
   addons: [
@@ -9,12 +12,51 @@ module.exports = {
     '@storybook/addon-viewport/register',
   ],
 
-  webpackFinal: async (config, {configType}) => {
-    config.module.rules.push({
-      test: /\.adoc/,
-      use: ["asciidoc-loader"],
-      include: path.resolve(__dirname, '../'),
+  webpackFinal: async (config, { configType }) => {
+    return merge(config, {
+
+      module: {
+        rules: [
+          {
+            test: /\.adoc/,
+            use: ['asciidoc-loader'],
+            include: path.resolve(__dirname, '../'),
+          }
+        ]
+      },
+      optimization: {
+        splitChunks: {
+          chunks: 'all',
+          minSize: 30 * 1024,
+          maxSize: maxAssetSize,
+          cacheGroups: {
+            commons: {
+              test: /[\\/]node_modules[\\/]/,
+              // cacheGroupKey here is `commons` as the key of the cacheGroup
+              name(module, chunks, cacheGroupKey) {
+                const moduleFileName = module.identifier().split('/').reduceRight(item => item);
+                const allChunksNames = chunks.map((item) => item.name).join('~');
+                return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
+              },
+              chunks: 'all'
+            },
+            components: {
+              test: /[\\/]src[\\/]/,
+              // cacheGroupKey here is `components` as the key of the cacheGroup
+              name(module, chunks, cacheGroupKey) {
+                const moduleFileName = module.identifier().split('/').reduceRight(item => item);
+                const allChunksNames = chunks.map((item) => item.name).join('~');
+                return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
+              },
+              chunks: 'all'
+            }
+          }
+        }
+      },
+      performance: {
+        maxAssetSize: maxAssetSize
+      }
     });
-    return config;
   },
 };
+
