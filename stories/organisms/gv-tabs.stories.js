900f01d0a7cfd82ca65b67af43455a908d70e7c1
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2021-01-26 06:18:25 +0100

    feat(gv-tabs): add slot title, small & truncate options to manage responsive of tabs

--- stories/organisms/gv-tabs.stories.js
+++ stories/organisms/gv-tabs.stories.js
@@ -30,11 +30,45 @@ const conf = {
   component: 'gv-tabs',
 };
 
-export const Basics = makeStory(conf, {
+export const Simple = makeStory(conf, {
   items: [{
-    options: [{ id: 'one', title: 'Tab 1' }, { id: 'two', title: 'Tab 2' }],
+    options: ['tab-1', 'tab-2', 'tab-3'],
+    value: 'one',
+    innerHTML: '<div slot="content" id="tab-1">Content One</div>'
+      + '<div slot="content" id="tab-2">Content two</div>'
+      + '<div slot="content" id="tab-3">Content two</div>',
+  }],
+});
+
+export const WithTitle = makeStory(conf, {
+  items: [{
+    options: [{ id: 'one', title: 'Tab 1' }, { id: 'two', title: 'Tab 2' }, { id: 'three', title: 'Tab 3' }],
     value: 'one',
     innerHTML: '<div slot="content" id="one">Content One</div>'
-      + '<div slot="content" id="two">Content two</div>',
+      + '<div slot="content" id="two">Content two</div>'
+      + '<div slot="content" id="three">Content three</div>'
+      + '<div slot="title">My tabs</div>',
+  }],
+});
+
+export const WithValidator = makeStory(conf, {
+  items: [{
+    options: [{ id: 'one' }, { id: 'two' }, { id: 'three' }],
+    value: 'two',
+    innerHTML: '<div slot="content" id="one">You can open tab <b>two</b> or <b>three</b></div>'
+      + '<div slot="content" id="two">You can only open tab <b>one</b></div>'
+      + '<div slot="content" id="three">You can open tab <b>one</b> or <b>two</b></div>',
+    validator: ({ from, to }) => {
+
+      if (from === 'two' && to === 'three') {
+        const reason = `[Validator] Cannot change tab from[${from}] to[${to}]`;
+        // eslint-disable-next-line no-console
+        console.log(reason);
+        return Promise.reject(reason);
+      }
+      // eslint-disable-next-line no-console
+      console.log(`[Validator] Can change tab from[${from}] to[${to}]`);
+      return Promise.resolve();
+    },
   }],
 });
