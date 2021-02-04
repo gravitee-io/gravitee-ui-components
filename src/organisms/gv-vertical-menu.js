6578fe3f8f51364638d44d204f8b8183d0398ca2
Lorie Pisicchio <lorie.pisicchio@gmail.com>
2021-02-04 15:19:31 +0100

    feat(): reduce menu item height

--- src/organisms/gv-vertical-menu.js
+++ src/organisms/gv-vertical-menu.js
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { css, html, LitElement } from 'lit-element';
-import '../molecules/gv-nav';
-import '../atoms/gv-image';
-import { isSameRoutes } from '../lib/utils';
+import { css, html, LitElement } from "lit-element";
+import "../molecules/gv-nav";
+import "../atoms/gv-image";
+import { isSameRoutes } from "../lib/utils";
 
 /**
  * A vertical menu
@@ -27,8 +27,7 @@ import { isSameRoutes } from '../lib/utils';
  *
  */
 export class GvVerticalMenu extends LitElement {
-
-  static get properties () {
+  static get properties() {
     return {
       /** @required **/
       routes: { type: Array },
@@ -37,23 +36,32 @@ export class GvVerticalMenu extends LitElement {
     };
   }
 
-  static get styles () {
+  static get styles() {
     return [
       // language=css
       css`
         :host {
-          --gv-link--c: var(--gv-menu--c, var(--gv-theme-font-color-light, #FFFFFF));
-          --gv-link-active--c: var(--gv-menu--c, var(--gv-theme-font-color-light, #FFFFFF));
+          --gv-link--c: var(
+            --gv-menu--c,
+            var(--gv-theme-font-color-light, #ffffff)
+          );
+          --gv-link-active--c: var(
+            --gv-menu--c,
+            var(--gv-theme-font-color-light, #ffffff)
+          );
           --gv-link--bgc: var(--gv-menu-link--bgc, transparent);
           --gv-link-active--bgc: var(--gv-menu-link-active--bgc, transparent);
-          --gv-link-active--bdw: var(--gv-menu-link-active--bdw, 0 3px 0 0);
-          --gv-link-active--bdc: var(--gv-menu-link-active--bdc, var(--gv-theme-color-light, #86c3d0));
+          --gv-link-active--bdw: var(--gv-menu-link-active--bdw, 0 0 0 4px);
+          --gv-link-active--bdc: var(
+            --gv-menu-link-active--bdc,
+            var(--gv-theme-color-light, #86c3d0)
+          );
           --gv-link-active--bds: var(--gv-menu-link-active--bds, solid);
           --pr: var(--gv-theme-layout--pr, 4rem);
           --pl: var(--gv-theme-layout--pl, 4rem);
           box-sizing: border-box;
           display: block;
-          font-size: var(--gv-theme-font-size-s, 12px);
+          font-size: var(--gv-theme-font-size-s, 16px);
           width: var(--gv-theme-layout-menu-w, 200px);
         }
 
@@ -62,8 +70,11 @@ export class GvVerticalMenu extends LitElement {
           flex-direction: column;
           justify-content: space-between;
 
-          background-color: var(--gv-menu--bgc, var(--gv-theme-color-dark, #28444F));
-          color: var(--gv-menu--c, var(--gv-theme-font-color-light, #FFFFFF));
+          background-color: var(
+            --gv-menu--bgc,
+            var(--gv-theme-color-dark, #28444f)
+          );
+          color: var(--gv-menu--c, var(--gv-theme-font-color-light, #ffffff));
         }
 
         gv-image {
@@ -76,21 +87,22 @@ export class GvVerticalMenu extends LitElement {
           display: flex;
           flex-direction: column;
           justify-content: space-between;
-          line-height: 50px;
+          line-height: 20px;
           vertical-align: middle;
           width: 100%;
+          font-size: 16px;
         }
       `,
     ];
   }
 
-  constructor () {
+  constructor() {
     super();
     /** @protected */
     this._routes = [];
   }
 
-  set routes (candidate) {
+  set routes(candidate) {
     if (candidate) {
       Promise.resolve(candidate).then((candidates) => {
         if (candidates) {
@@ -99,27 +111,25 @@ export class GvVerticalMenu extends LitElement {
               this._routes = futureRoutes.filter((r) => r != null);
             }
           });
-        }
-        else {
+        } else {
           this._routes = null;
         }
       });
-    }
-    else {
+    } else {
       this._routes = null;
     }
   }
 
-  render () {
+  render() {
     return html`
-        <div class="menu-container">
-            ${(this.logo) ? html`<gv-image .src="${this.logo}" ></gv-image>` : ''}
-            <div class="nav-container">
-                <gv-nav .routes="${this._routes}" vertical></gv-nav>
-            </div>
+      <div class="menu-container">
+        ${this.logo ? html`<gv-image .src="${this.logo}"></gv-image>` : ""}
+        <div class="nav-container">
+          <gv-nav .routes="${this._routes}" vertical></gv-nav>
         </div>
+      </div>
     `;
   }
 }
 
-window.customElements.define('gv-vertical-menu', GvVerticalMenu);
+window.customElements.define("gv-vertical-menu", GvVerticalMenu);
