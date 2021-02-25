90029834b217a69e08e9a768924615d3acf7172d
Gaëtan Maisse <gaetanmaisse@gmail.com>
2021-02-25 09:06:22 +0100

    chore: add semantic-release

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        pkgRoot: 'dist',
+      },
+    ],
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
+};
