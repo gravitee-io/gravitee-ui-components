57316eb988f38cc792cda3142b734cb1e6f7c4dc
ouahid <ouahid.khelifi@gmail.com>
2021-05-04 15:00:40 +0200

    feat(gv-chart-histogram): add new component

---
+++ stories/charts/gv-chart-histogram.stories.js
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import notes from '../../.docs/gv-chart-histogram.md';
+import '../../src/charts/gv-chart-histogram';
+import { makeStory, storyWait } from '../lib/make-story';
+
+const series = {
+  values: [
+    {
+      name: '9ec7b0e2-a649-40a3-87b0-e2a649e0a377',
+      color: '#5cb85c',
+      data: [100.0, 80.0, 90.0, 100.0, 0, 0, 0, 20.0, 30.0, 50.0, 60.0, 70.0, 0],
+    },
+  ],
+};
+
+const options = {
+  yAxisTitle: 'Availability (%)',
+  title: 'Global availability',
+  subtitle: 'Global availability including results of all health-checked endpoints (see below).',
+  min: 0,
+  max: 100,
+  data: {
+    name: 'timestamp',
+    values: [
+      '20. Apr',
+      '21. Apr',
+      '22. Apr',
+      '23. Apr',
+      '24. Apr',
+      '26. Apr',
+      '27. Apr',
+      '28. Apr',
+      '29. Apr',
+      '30. Apr',
+      '1. May',
+      '2. May',
+      '3. May',
+    ],
+  },
+};
+
+export default {
+  title: 'charts/gv-chart-histogram',
+  component: 'gv-chart-histogram',
+  parameters: {
+    notes,
+    chromatic: { disable: true },
+  },
+};
+
+const conf = {
+  component: 'gv-chart-histogram',
+  css: `
+    gv-chart-histogram {
+      min-height: 200px;
+    }
+  `,
+};
+
+export const Basics = makeStory(conf, {
+  items: [{ series, options }],
+});
+
+export const Empty = makeStory(conf, {
+  items: [{ series: [], options }],
+});
+
+let seriesResolver;
+export const Loading = makeStory(conf, {
+  items: [{}],
+  simulations: [
+    storyWait(0, ([component]) => {
+      component.series = new Promise((resolve) => (seriesResolver = resolve));
+      component.options = options;
+    }),
+
+    storyWait(750, () => {
+      seriesResolver(series);
+    }),
+  ],
+});
