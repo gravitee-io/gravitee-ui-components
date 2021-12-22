bc74a4daa3f7df00126f0665b768aa63114a50f5
Thibaud Av <thibaud.avenier@gmail.com>
2021-12-22 23:01:30 +0100

    chore: allow storybook to work with typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,7 @@ module.exports = {
   features: {
     previewCsfV3: true,
   },
-  stories: ['../stories/**/*.stories.@(js|mdx)'],
+  stories: ['../stories/**/*.stories.@(js|ts|mdx)'],
 
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
 
