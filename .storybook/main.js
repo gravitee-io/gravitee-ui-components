59c3181d07e0ad5021537ab569926b4e3c3d74d0
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2022-01-05 14:38:08 +0100

    chore: resolve ts decorators integration

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,4 +5,14 @@ module.exports = {
   stories: ['../stories/**/*.stories.@(js|ts|mdx)', '../src/**/*.stories.@(js|ts|mdx)'],
 
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
+
+  babel: async (options) => {
+    options.plugins = options.plugins.map((plugin) => {
+      if (Array.isArray(plugin) && plugin[0].includes('plugin-proposal-decorators')) {
+        plugin[1] = { decoratorsBeforeExport: true };
+      }
+      return plugin;
+    });
+    return options;
+  },
 };
