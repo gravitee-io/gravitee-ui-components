3749894faa0cc7d489a903e86d6b08755f8a9935
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2026-09-10 15:03:24 +0200

    chore(deps): move Babel to 8
    The decorators plugin now takes its mode as a `version` option rather than a `legacy`
    flag, in the Jest configuration and in the i18n task alike.

--- tasks/check-i18n.js
+++ tasks/check-i18n.js
@@ -34,7 +34,7 @@ async function getUsedKeys(sourceFilepaths) {
         ast: true,
         plugins: [
           '@babel/plugin-transform-typescript',
-          ['@babel/plugin-proposal-decorators', { legacy: true }],
+          ['@babel/plugin-proposal-decorators', { version: 'legacy' }],
           ['@babel/plugin-transform-class-properties', { loose: true }],
           ['@babel/plugin-transform-private-property-in-object', { loose: true }],
         ],
